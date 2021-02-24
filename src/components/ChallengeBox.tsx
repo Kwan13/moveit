ef240cca1a471f200cdf0345b4e7c9021348515c
Kwan13 <kawan-silva@outlook.com>
2021-02-24 13:52:42 -0300

    add 'ChallengeBox' component

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,42 @@
+import styles from '../styles/components/ChallengeBox.module.css';
+
+export function ChallengeBox() {
+
+  const hasAcitiveChallenge = true;
+
+  return (
+    <div className={styles.challengeBoxContainer}>
+      {hasAcitiveChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400 xp</header>
+
+          <main>
+            <img src="icons/body.svg" />
+            <strong>Novo desafio</strong>
+            <p>Levande e faça um caminhada de 3 minutos.</p>
+          </main>
+
+          <footer>
+            <button
+              type="button"
+              className={styles.challengeFailButton}
+            >Falhei</button>
+
+            <button
+              type="button"
+              className={styles.challengeSucceededButton}
+            >Completei</button>
+          </footer>
+        </div>
+      ) : (
+          <div className={styles.challengeNotActive}>
+            <strong>Finalize um ciclo para receber um desafio</strong>
+            <p>
+              <img src="icons/level-up.svg" alt="Level Up" />
+          Avance de level completando desafios.
+        </p>
+          </div>
+        )}
+    </div>
+  )
+}
